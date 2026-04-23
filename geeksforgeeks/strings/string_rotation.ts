d00c8d460cddb147c09f099360759df4e017739d
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2026-04-23 20:50:14 +0530

    Update string_rotation.ts

--- geeksforgeeks/strings/string_rotation.ts
+++ geeksforgeeks/strings/string_rotation.ts
@@ -1,8 +1,9 @@
+// Problem link  - https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-gfg-160/problem/check-if-strings-are-rotations-of-each-other-or-not-1587115620
+
 class String_Rotation {
     areRotations(s1:string, s2:string):bool {
         if(s2.length > s1.length)
             return false
-        
         const doubled = s1 + s1
 
         return doubled.includes(s2)
